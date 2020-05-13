00290c81fd4918a4cf4cdf8f47c8f5f9af343ed6
jinweisu <aogueisu@gmail.com>
2020-05-13 20:13:22 +0800

    Update main.js

--- main.js
+++ main.js
@@ -50,7 +50,7 @@ function run() {
     var queue = 0;
     //第一位客人的開始時間等於他到達的時間
     var start_time = arrival_time;
-    var str = "<table border='1'> <tr><td>客戶編號</td><td>排隊人數</td><td>誰正在被服務中</td><td>到達時間</td><td>開始時間</td><td>服務結束</td><td>服務時間</td><td>其他資訊</td></tr>";
+    var str = "<table border='1'> <tr><td>Customer Num.</td><td>Number in queue</td><td>Who is being served</td><td>arrival time</td><td>Start time</td><td>End time</td><td>Service time</td><td>other info.</td></tr>";
     var end_time = 0;
     var min_end_time = 1;
     var who_service_now = 0;
